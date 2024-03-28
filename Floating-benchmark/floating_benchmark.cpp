1872fde24cd43f405b224942af62b13dfb26bbb4
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-28 12:23:51 -0400

    Added 10^10 additions on floating benchmark

--- Floating-benchmark/floating_benchmark.cpp
+++ Floating-benchmark/floating_benchmark.cpp
@@ -5,4 +5,11 @@ int main() {
     // getting the stat time
     auto start = std::chrono::high_resolution_clock::now();
 
+    // 10^10 additions
+    double sum = 0.0;
+    for (long long i = 0; i < 10000000000LL; i++) {
+        sum += 1.0;
+    }
+
+    
 }

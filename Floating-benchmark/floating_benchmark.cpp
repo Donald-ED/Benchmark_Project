fd65f3d1c4bc4c3673b40f674f31aeb18aa2cc72
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-28 12:28:15 -0400

    Added Multiplication and Division operations to FB

--- Floating-benchmark/floating_benchmark.cpp
+++ Floating-benchmark/floating_benchmark.cpp
@@ -11,5 +11,15 @@ int main() {
         sum += 1.0;
     }
 
-    
+    // 5 x 10^9 Multiplications
+    double product = 1.0;
+    for (long long i = 0; i < 5000000000LL; i++) {
+        product *= 2.0;
+    }
+
+    // 2 x 10^9 Divisions
+    double quotient = 100.0;
+    for (long long i = 0; i < 2000000000LL; i++) {
+        quotient /= 2.0;
+    }
 }

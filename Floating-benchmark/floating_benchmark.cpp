e323d6d90b52ce9c0ad0d9c7f9174e56b0fddbab
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-28 18:32:05 -0400

    Floating point bench working now.

--- Floating-benchmark/floating_benchmark.cpp
+++ Floating-benchmark/floating_benchmark.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 
 int main() {
-    // getting the stat time
+    // Getting the start time
     auto start = std::chrono::high_resolution_clock::now();
 
     // 10^10 additions
     double sum = 0.0;
-    for (int i = 0; i < 10000000000LL; i++) {
+    for (long long i = 0; i < 10000000000LL; i++) {
         sum += 1.0;
     }
 
     // 5 x 10^9 Multiplications
     double product = 1.0;
-    for (int i = 0; i < 5000000000LL; i++) {
+    for (long long i = 0; i < 5000000000LL; i++) {
         product *= 2.0;
     }
 
     // 2 x 10^9 Divisions
     double quotient = 100.0;
-    for (int i = 0; i < 2000000000LL; i++) {
+    for (long long i = 0; i < 2000000000LL; i++) {
         quotient /= 2.0;
     }
+    
+    // Getting the end time
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> float_ms = end - start;
+    std::cout << "64-bit Floating Point Operation Benchmark completed in " << float_ms.count() / 1000 << " seconds." << std::endl;
+
+    return 0;
 }

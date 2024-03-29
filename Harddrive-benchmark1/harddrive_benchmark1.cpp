03e9b5ce0019224ef189ff9026a61e24780e42d0
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-28 21:54:46 -0400

    Getting start time for harddrive B1

--- Harddrive-benchmark1/harddrive_benchmark1.cpp
+++ Harddrive-benchmark1/harddrive_benchmark1.cpp
@@ -4,5 +4,11 @@
 #include <vector>
 
 int main() {
-    
+    const std::string filename = "Harddrive-benchmark1/temp_file.bin";
+    std::vector<char> writebuffer(100, 'A'); // 100 bytes of 'A'
+    std::vector<char> readbuffer(100); // 100 bytes for reading
+
+    // Getting start time
+    auto start = std::chrono::high_resolution_clock::now();
+
 }

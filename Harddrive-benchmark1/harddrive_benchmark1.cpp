6311feff88c6c144f2862880f7541b97c8a9d274
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-29 18:43:24 -0400

    Writing and reading from file.

--- Harddrive-benchmark1/harddrive_benchmark1.cpp
+++ Harddrive-benchmark1/harddrive_benchmark1.cpp
@@ -5,10 +5,25 @@
 
 int main() {
     const std::string filename = "Harddrive-benchmark1/temp_file.bin";
-    std::vector<char> writebuffer(100, 'A'); // 100 bytes of 'A'
-    std::vector<char> readbuffer(100); // 100 bytes for reading
+    std::vector<char> writeBuffer(100, 'A'); // 100 bytes of 'A'
+    std::vector<char> readBuffer(100); // 100 bytes for reading
 
     // Getting start time
     auto start = std::chrono::high_resolution_clock::now();
+
+    // Writing to file
+    std::ofstream outfile(filename, std::ofstream::binary);
+    for(long long i = 0; i < 10000000; ++i) {
+        outfile.write(writeBuffer.data(),  readBuffer.size());
+
+    }
+    outfile.close();
+
+    // Reading from file
+    std::ifstream infile(filename, std::ifstream::binary);
+    while (infile.read(readBuffer.data(), readBuffer.size())){
+        // Simulate Processing (to prevent optimizitions that skip reading)
+    }
+    infile.close();
 
 }

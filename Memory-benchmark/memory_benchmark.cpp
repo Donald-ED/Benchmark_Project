f412492ec2074480e8936b206c86a191b84b6c30
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-30 12:43:36 -0400

    Reading from the Array. Using Long long Var

--- Memory-benchmark/memory_benchmark.cpp
+++ Memory-benchmark/memory_benchmark.cpp
@@ -5,7 +5,13 @@
 int main() {
     std::vector<int> vec(5000000000LL, 1); // 5 x 10^9 elements
     
-    //Getting start time
+    // Getting start time
     auto start = std::chrono::high_resolution_clock::now();
+
+    // Reading from array
+    long long read_sum = 0;
+    for(long long i = 0; i < 5000000000LL; ++i) {
+        read_sum += vec[i];
+    }
 
 }

c61519b40d047f9ce402a5cd549db2390d54746e
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-29 20:13:07 -0400

    Add 10^10 addition operation

--- Integer-benchmark/integer_benchmark.cpp
+++ Integer-benchmark/integer_benchmark.cpp
@@ -4,4 +4,12 @@
 int main() {
     // Getting start time
     auto start = std::chrono::high_resolution_clock::now();
+
+    // 10^10 additions
+    int sum = 0;
+    for(long long i = 0; i < 10000000000LL; ++i) {
+        sum += 1; // Simple addition
+    }
+
+    
 }

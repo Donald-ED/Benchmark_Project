3d81af1114a579dbe57f008da63c0a0f0a928c3e
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-29 20:29:32 -0400

    Code completed but the seconds are not correct

--- Integer-benchmark/integer_benchmark.cpp
+++ Integer-benchmark/integer_benchmark.cpp
@@ -22,4 +22,11 @@ int main() {
     for (long long i = 0; i < 2000000000LL; ++i) {
         quotient /= 2;
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> float_ms = end - start;
+
+    std::cout<< "32-bit Integer Operation Benchmark completed in "<< float_ms.count() << " seconds." <<std::endl;
+
+    return 0; 
 }

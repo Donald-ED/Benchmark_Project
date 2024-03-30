6ff5f4ec56457ce38a3b110e09cd31d2a4c43030
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-03-29 20:22:00 -0400

    Multiplication and Division operation.

--- Integer-benchmark/integer_benchmark.cpp
+++ Integer-benchmark/integer_benchmark.cpp
@@ -11,5 +11,15 @@ int main() {
         sum += 1; // Simple addition
     }
 
-    
+    // 5 x 10^9 multiplications
+    int product = 1;
+    for(long long i = 0; i < 5000000000LL; ++i) {
+        product *= 2;
+    }
+
+    // 2 x 10^9 divisions
+    int quotient = 100;
+    for (long long i = 0; i < 2000000000LL; ++i) {
+        quotient /= 2;
+    }
 }
